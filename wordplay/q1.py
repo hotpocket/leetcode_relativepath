a4602ea80afd04d5d0046132d8318e519c0c6a99
hotpocket <githotpocket@gmail.com>
2025-09-17 10:32:29 -0700

    first question! huzzah!

--- wordplay/q1.py
+++ wordplay/q1.py
@@ -1,10 +1,17 @@
-
 # For loops and if conditions
 # What are all of the words containing UU?
 
+# iterate lines in file
+# if line contains the string "UU" add it to a running list
+# print the list
+
 words_file = 'sowpods.txt'
 with open(words_file) as fh:
-  for line in fh:
-    print(line)
+  try:
+    for line in fh:
+      if "UU" in line:
+        print(line.strip())
+  except BrokenPipeError:
+    pass # don't complain on things like `python q1.py | head`
 
 
